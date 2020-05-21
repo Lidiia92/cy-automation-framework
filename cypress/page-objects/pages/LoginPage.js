8029778f379c83dcd4f00e9c92235b0f2f95152c
Lidii <lidiiany92@gmail.com>
2020-05-20 18:10:13 -0700

    added login page class

---
+++ cypress/page-objects/pages/LoginPage.js
@@ -0,0 +1,15 @@
+export default class LoginPage extends BasePage {
+	static login(username, password) {
+		cy.login(username, password);
+	}
+
+	static clickForgotPasswordLink() {
+		cy.contains('Forgot your password ?').click();
+	}
+
+	static displayErrorMessage() {
+		// cy.get('.alert-error').should('be.visible');
+		//Custom command
+		cy.isVisible('.alert-error');
+	}
+}
